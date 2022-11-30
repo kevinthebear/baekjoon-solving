82c0f735ba1b9c6db1d3c80b1360a1ccff95e833
Kevin Shin <118714261+snukevin@users.noreply.github.com>
2022-11-30 11:35:44 +0900

    Create 18108.java

---
+++ Java/18108.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        int christian_era = in.nextInt();
+        
+        System.out.println(christian_era-543);
+    }
+}
