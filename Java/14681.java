92c8e8b4e6f87b94ea52d2d97705771847772e09
Kevin Shin <118714261+snukevin@users.noreply.github.com>
2022-11-30 14:28:00 +0900

    Create 14681.java

---
+++ Java/14681.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        int X = in.nextInt();
+        int Y = in.nextInt();
+        
+        if (X > 0 && Y > 0){
+            System.out.println("1");
+        }
+        else if (X < 0 && Y > 0){
+            System.out.println("2");
+        }
+        else if (X < 0 && Y < 0){
+            System.out.println("3");
+        }
+        else {
+            System.out.println("4");
+        }
+    }
+}
